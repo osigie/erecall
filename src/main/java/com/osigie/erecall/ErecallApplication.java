f14efc25ed11ad3f2d60a581d93de7139c8e5111
osigie <kenosagie88@gmail.com>
2026-06-24 05:05:43 +0100

    init

---
+++ src/main/java/com/osigie/erecall/ErecallApplication.java
@@ -0,0 +1,13 @@
+package com.osigie.erecall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErecallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErecallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osigie/erecall/ErecallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osigie.erecall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErecallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
