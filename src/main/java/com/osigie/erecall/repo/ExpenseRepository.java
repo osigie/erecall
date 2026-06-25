a2099ac2b9ad00bf9c1b09e47e4ace8c7d1f1105
osigie <kenosagie88@gmail.com>
2026-06-25 13:52:58 +0100

    update column name in query

--- src/main/java/com/osigie/erecall/repo/ExpenseRepository.java
+++ src/main/java/com/osigie/erecall/repo/ExpenseRepository.java
@@ -4,21 +4,14 @@ import com.osigie.erecall.domain.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
-    List<Expense> findByDateBetween(LocalDate startDate, LocalDate endDate);
-
-    List<Expense> findByMerchant(String merchant);
-
-    List<Expense> findByAmount(BigDecimal amount);
-
-    List<Expense> findByCategory(String category);
+    List<Expense> findByExpenseDateBetween(OffsetDateTime startDate, OffsetDateTime endDate);
 
     Optional<Expense> findByExpenseDocumentId(UUID expenseDocumentId);
 }
