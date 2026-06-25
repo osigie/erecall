5ab406c70e51d70e2ca2f642f07ae7342dee4215
osigie <kenosagie88@gmail.com>
2026-06-25 08:35:51 +0100

    add tools class

---
+++ src/main/java/com/osigie/erecall/service/query/ExpenseTools.java
@@ -0,0 +1,10 @@
+package com.osigie.erecall.service.query;
+
+public class ExpenseTools {
+
+    //create tools for the 3 categories
+    /*
+    * 1. LLM reasoning
+    * 2. SQL
+    * 3. Qdrant*/
+}
